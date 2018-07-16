9e8b7a261febacd348f1a2281a1dd682c81debf2
DESKTOP-74ERUTD\Jorge del Angel <soporte@sistemasags.com>
2018-07-16 12:50:17 -0500

    -

--- Camara/src/pages/login/login.ts
+++ Camara/src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { RestProvider } from '../../providers/rest/rest';
 
 /**
  * Generated class for the LoginPage page.
